d243179aa8b1dce436d4d9640e258144b69b1bf2
hussein guettaf <ghoucine@gmail.com>
2014-08-26 04:16:34 +0100

    Subfolder installs support added

--- public_html/index.php
+++ public_html/index.php
@@ -5,11 +5,14 @@ require "../vendor/phiber/phiber/library/wire.php";
 $phiber = new Phiber\wire('../application/config.php');
 /**
  *
- * $phiber->addroute('/text',function($phiber){
+ * $phiber->addRoute('/text',function($phiber){
  *
  *    echo 'Plain text';
  *
  * });
+ *
+ * //If installed in a subfolder (don't forget to change your .htaccess)
+ * $phiber->baseUri('subfolder');
  *
  */
 
